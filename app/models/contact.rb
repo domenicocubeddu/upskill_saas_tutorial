4851af9e0f7ba43cfd79c68535da4d3567ef37f0
Domenico Cubeddu <domenicocubeddu@live.it>
2020-07-11 17:29:03 +0000

    Updated error and success messages

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,5 @@
 class Contact < ActiveRecord::Base 
   validates :name, presence: true
   validates :email, presence: true
-  validates :comments, presence: true
+  validates :comments, presence: true, length: {maximum: 500}
 end
