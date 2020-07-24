5a41a83cd4dac0f083ed98403d99dc8f7a01f2fc
Domenico Cubeddu <domenicocubeddu@live.it>
2020-07-24 16:42:35 +0000

    Added comments to files

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,8 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  # Extend default devise gem behavioyr so that
+  # users signing up with the Pro account (plan ID 2)
+  # save with a specialist Stripe subscription function.
+  # Otherwise Devise signs up user as usual.
   def creat
     super do |resource|
       if params[:plan] 
